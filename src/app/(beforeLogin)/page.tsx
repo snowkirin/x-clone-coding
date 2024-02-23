6acc8d3fd7eb18dc5a7527c206bf83cae3734a88
osean-jim <119638097+osean-jim@users.noreply.github.com>
2024-02-23 17:40:25 +0900

    First Commit

--- src/app/(beforeLogin)/page.tsx
+++ src/app/(beforeLogin)/page.tsx
@@ -1,11 +1,5 @@
-import Link from "next/link";
-import Main from "@/app/(beforeLogin)/_component/Main";
-
+import Main from '@/app/(beforeLogin)/_component/Main';
 
 export default function Home() {
-  return (
-      <>
-        <Main />
-      </>
-  )
+  return <Main />;
 }
